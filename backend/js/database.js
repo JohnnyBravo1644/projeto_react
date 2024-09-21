76b4890977bc7f475f2220ca5825442d090e8fbb
JohnnyBravo1644 <joavitorjuv@outlook.com>
2024-09-20 22:34:45 -0300

    criado o código que fara a conexao com o banco de dados

---
+++ backend/js/database.js
@@ -0,0 +1,15 @@
+const { Pool } = require('pg');
+
+try {
+  const pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'tela_login',
+    password: '5068',
+    port: 5432 // porta padrão do PostgreSQL
+  });
+
+  module.exports = pool;
+} catch (error) {
+  console.error('Erro ao conectar ao banco de dados:', error);
+}
