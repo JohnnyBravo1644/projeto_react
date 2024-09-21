3eb3420c711b0fd79feca37259cc0a529d7f0344
JohnnyBravo1644 <joavitorjuv@outlook.com>
2024-09-21 19:28:01 -0300

    adicionados novos status que serao devolvidos para o frontend

--- backend/js/server.js
+++ backend/js/server.js
@@ -1,18 +1,18 @@
-import express, { request } from 'express';
+import express, { request } from "express";
 
 const app = express();
 app.use(express.json());
 
-const users = []
+const users = [];
 
-app.post('/usuarios', (req, res) => {
-    users.push(req.body)
+app.post("/usuarios", (req, res) => {
+  users.push(req.body);
 
-    res.send('ok post')
-})
+  res.status(201).json(req.body);
+});
 
-app.get('/usuarios', (req, res) => {
-    res.json(users)
-})
+app.get("/usuarios", (req, res) => {
+  res.status(200).json(users);
+});
 
 app.listen(3000);
